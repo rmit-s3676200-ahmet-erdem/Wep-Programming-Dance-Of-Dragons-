254319e5a465182ef6dd8d81f092e7b1a276b766
erdemah <s3676200@student.rmit.edu.au>
2017-10-15 23:53:47 +1100

    submit

--- registration.php
+++ registration.php
@@ -1,3 +1,4 @@
+<!-- codes were adapted from StackOverFlow and PHP Manual -->
 <?php
   session_start();
    // include_path='C:\xampp\php\PEAR'
@@ -5,6 +6,7 @@
   $counter = 0;
   if (isset($_POST["ticketSubmit"])) {
     $_SESSION['cart'][]=$_POST;
+    header("Location:printableTickets.php");
   } 
   //unset ($_SESSION['cart']);
   // session_unset();
@@ -17,10 +19,10 @@
     <?php require_once("top-module.php"); ?>
   </head>
    <?php require_once("header-nav-module.php"); ?>
-<main>
-
-	<div class="container">
-		<form method='post' id='registration' class='form' method='post' onsubmit='return formValidation();'>
+   <body>
+    <main>
+	   <div class="container">
+		  <form method='post' id='registration' class='form' method='post' onsubmit='return formValidation();'>
 			<input name="source" id="source" type="hidden" class="form-control" value="RegisterUser">
 			<label for="name">Name: </label>
 			<input name="name" id="name" type="text" placeholder="John" required>
@@ -34,12 +36,9 @@
 			<input name="phNo" id="phNo" type="text" required>
 			<span class='error' id='phNoError'></span>
 			<input name="ticketSubmit" id="sub" type="submit">
-		</form>		
-	</div>
-</body>
-</html>
-
-	</main>
+		  </form>		
+	   </div>
+	  </main>
 <?php require_once("footer-module.php"); ?>
 <?php include_once("/home/eh1/e54061/public_html/wp/debug.php"); ?>
   <script type="text/javascript" src="formValidate.js?v=2"></script>
